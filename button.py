c2bd717ca9f0592a77e8ac39112d0bafd544887d
unknown <wuge_1996@qq.com>
2019-11-18 16:13:47 +0800

    first commit

---
+++ button.py
@@ -0,0 +1,36 @@
+import pygame.font
+
+class Button():
+
+    def __init__(self, ai_settings, screen, msg):
+        """初始化按钮的属性"""
+        self.screen = screen
+        self.screen_rect = screen.get_rect()
+
+        # 设置按钮的尺寸和其他属性
+        self.width, self.height = 200, 50
+        self.button_color = (0, 255, 0)
+        self.text_color = (255, 255, 255)
+        self.font = pygame.font.SysFont('arial', 48)
+
+        # 创建按钮的rect对象，并使其在左上角
+        self.rect = pygame.Rect(0, 0, self.width, self.height)
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.left = self.screen_rect.left
+
+        # 按钮的标签只需创建一次
+        self.prep_msg(msg)
+
+
+    def prep_msg(self, msg):
+        """将msg渲染成图像，并使其在按钮上居中"""
+        self.msg_image = self.font.render(msg, True, self.text_color,
+            self.button_color)
+        self.msg_image_rect = self.msg_image.get_rect()
+        self.msg_image_rect.center = self.rect.center
+
+
+    def draw_button(self):
+        # 绘制一个用颜色填充的按钮，再绘制文本
+        self.screen.fill(self.button_color, self.rect)
+        self.screen.blit(self.msg_image, self.msg_image_rect)
